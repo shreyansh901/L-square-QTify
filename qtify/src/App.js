ced8556cb7099f5b58118f1475742564790d6c29
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2024-05-08 15:02:38 +0530

    HeroSection

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 import Navbar from "./components/NavBar/NavBar.jsx";
+import HeroSection from "./components/HeroSection/hero.jsx";
 
 function App(){
    return(
      <>
        <StyledEngineProvider injectFrist>
           <Navbar/>
+          <HeroSection />
        </StyledEngineProvider>
      </>
     );
